a7e8ab3246bf24fa2d4638b6f60c1cc1c616cdc2
aburiad <aburiad507@gmail.com>
2024-11-01 11:55:58 +0600

    add my_books_include_assets() load all assets

--- wp-my-book.php
+++ wp-my-book.php
@@ -23,3 +23,26 @@ if (!defined('MY_BOOKS_URL')) {
 if (!defined('MY_BOOKS_VERSION')) {
     define('MY_BOOKS_VERSION', '1.0');
 }
+
+/*
+ * Load my-books plugin
+ * all js and css
+ *
+ * */
+function my_books_include_assets()
+{
+    wp_enqueue_style('bootstrap', MY_BOOKS_URL . 'assets/css/bootstrap.min.css', '', MY_BOOKS_VERSION, 'all');
+    wp_enqueue_style('datatables', MY_BOOKS_URL . 'assets/css/dataTables.dataTables.min.css', '', MY_BOOKS_VERSION, 'all');
+    wp_enqueue_style('notifybar', MY_BOOKS_URL . 'assets/css/jquery.notifyBar.css', '', MY_BOOKS_VERSION, 'all');
+    wp_enqueue_style('custom', MY_BOOKS_URL . 'assets/css/custom.css', '', MY_BOOKS_VERSION, 'all');
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', MY_BOOKS_URL . 'assets/js/bootstrap.bundle.min.js', '', MY_BOOKS_VERSION, true);
+    wp_enqueue_script('validate', MY_BOOKS_URL . 'assets/js/jquery.validate.min.js', '', MY_BOOKS_VERSION, true);
+    wp_enqueue_script('dataTables', MY_BOOKS_URL . 'assets/js/dataTables.min.js', '', MY_BOOKS_VERSION, true);
+    wp_enqueue_script('notifyBar', MY_BOOKS_URL . 'assets/js/jquery.notifyBar.js', '', MY_BOOKS_VERSION, true);
+    wp_enqueue_script('custom', MY_BOOKS_URL . 'assets/js/custom.js', '', MY_BOOKS_VERSION, true);
+
+}
+
+add_action('init', 'my_books_include_assets');

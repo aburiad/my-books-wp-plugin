05ee0fc9e490692052f8f5f5fc718d09dea61890
aburiad <aburiad507@gmail.com>
2024-11-11 21:25:33 +0600

    login,logout state change href

--- views/frontend-booklist.php
+++ views/frontend-booklist.php
@@ -1,6 +1,7 @@
 <?php
 
 global $wpdb;
+global $user_ID;
 $table_name = my_book_table();
 $books_results = $wpdb->get_results("SELECT * FROM `$table_name`", ARRAY_A);
 
@@ -11,7 +12,20 @@ foreach ($books_results as $key => $value) {
         <img src="<?php echo $value['book_image'] ?>" alt="no image found" style="width: 100%">
         <h3><?php echo $value['author']; ?></h3>
         <p><?php echo $value['name']; ?></p>
-        <button type="button">Enroll</button>
+        <a class="btn btn-success" href="<?php if($user_ID > 0){echo "javascript:void"; }else{echo wp_login_url();}?>" type="button">
+            <?php
+            if ($user_ID > 0) {
+                ?>
+                Enroll
+                <?php
+            } else {
+                ?>
+                Login to Enroll
+                <?php
+            }
+            ?>
+
+        </a>
     </div>
     <?php
 }
